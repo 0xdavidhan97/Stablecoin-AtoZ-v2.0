9b54597623ce8742c6641206ddb12cba79c77589
한정우 <han@hanjeong-uui-noteubug.local>
2026-05-03 02:21:30 +0900

    refactor: extract TopBanner component from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Sidebar from '@/components/Sidebar'
+import TopBanner from '@/components/TopBanner'
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white pt-[71px]">
       <Sidebar />
       <main className="xl:ml-[240px] bg-white min-h-[calc(100vh-71px)]">
-        <div className="mt-[8px] mx-[8px] h-[88px] bg-[#FAFAFA] rounded-[5px]" />
+        <TopBanner />
       </main>
     </div>
   )
---
+++ components/TopBanner.tsx
@@ -0,0 +1,5 @@
+export default function TopBanner() {
+  return (
+    <div className="mt-[8px] mx-[8px] h-[88px] bg-[#FAFAFA] rounded-[5px]" />
+  )
+}
