adb6525b8c695f220cfe9014f9d1ad643a0704e6
한정우 <han@hanjeong-uui-noteubug.local>
2026-05-03 02:08:57 +0900

    feat: add main content box (1185x88, top:78px page-based)

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-white pt-[71px]">
       <Sidebar />
-      <main className="xl:ml-[240px] bg-white min-h-[calc(100vh-71px)]" />
+      <main className="xl:ml-[240px] bg-white min-h-[calc(100vh-71px)] relative">
+        <div className="absolute top-[7px] left-[248px] w-[1185px] h-[88px] bg-[#FAFAFA] rounded-[5px]" />
+      </main>
     </div>
   )
 }
