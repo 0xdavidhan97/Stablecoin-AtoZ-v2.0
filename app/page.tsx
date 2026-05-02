7beb58c5f92bcf413b488a756ceed576412ed611
한정우 <han@hanjeong-uui-noteubug.local>
2026-05-03 03:00:20 +0900

    fix: add border, background to Group 22 card and nest text/line inside

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,13 @@ export default function Home() {
       <main className="xl:ml-[240px] bg-white min-h-[calc(100vh-71px)] relative">
         <TopBanner />
 
-        {/* Group container */}
-        <div className="absolute top-[108px] left-[33px] w-[548px] h-[374px]" />
-
-        {/* Line 10 */}
-        <div className="absolute top-[150px] left-[33px] w-[548px] h-0 border-t border-[#F2F2F2]" />
-
-        {/* Stablecoin Market Cap */}
-        <p className="absolute top-[120px] left-[50px] w-[163px] h-[18px] text-[15px] font-medium leading-[18px] text-black">
-          Stablecoin Market Cap
-        </p>
+        {/* Group 22 */}
+        <div className="absolute top-[108px] left-[33px] w-[548px] h-[374px] bg-white border border-[#F2F2F2] rounded-[5px] relative">
+          <p className="absolute top-[12px] left-[17px] w-[163px] h-[18px] text-[15px] font-medium leading-[18px] text-black">
+            Stablecoin Market Cap
+          </p>
+          <div className="absolute top-[42px] left-0 w-full h-0 border-t border-[#F2F2F2]" />
+        </div>
       </main>
     </div>
   )
