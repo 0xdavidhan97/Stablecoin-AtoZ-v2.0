3cdf9efe4c93ebe9aa46ee6bd7336bb9cf20e337
한정우 <han@hanjeong-uui-noteubug.local>
2026-05-03 02:15:52 +0900

    fix: update main box to fluid width with 8px margins

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-white pt-[71px]">
       <Sidebar />
-      <main className="xl:ml-[240px] bg-white min-h-[calc(100vh-71px)] relative">
-        <div className="absolute top-[7px] left-[248px] w-[1185px] h-[88px] bg-[#FAFAFA] rounded-[5px]" />
+      <main className="xl:ml-[240px] bg-white min-h-[calc(100vh-71px)]">
+        <div className="mt-[8px] mx-[8px] h-[88px] bg-[#FAFAFA] rounded-[5px]" />
       </main>
     </div>
   )
