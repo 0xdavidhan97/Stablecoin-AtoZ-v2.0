05c12e53360ec58c239ca4d684b789e6e8640548
한정우 <han@hanjeong-uui-noteubug.local>
2026-05-03 02:27:22 +0900

    feat: add 3 text elements to TopBanner

--- components/TopBanner.tsx
+++ components/TopBanner.tsx
@@ -1,5 +1,15 @@
 export default function TopBanner() {
   return (
-    <div className="mt-[8px] mx-[8px] h-[88px] bg-[#FAFAFA] rounded-[5px]" />
+    <div className="relative mt-[8px] mx-[8px] h-[88px] bg-[#FAFAFA] rounded-[5px]">
+      <span className="absolute top-[10px] left-[17px] w-[158px] h-[15px] text-[13px] font-bold leading-[16px] tracking-[0.1em] text-[#5F5F5F]">
+        Stablecoin A to Z란?
+      </span>
+      <span className="absolute top-[33px] left-[34px] w-[697px] h-[15px] text-[13px] font-medium leading-[16px] tracking-[0.1em] text-[#5F5F5F]">
+        스테이블코인과 블록체인에 관심이 많은 쟁쟁이가 만든 스테이블코인 온체인 데이터 대시보드입니다.
+      </span>
+      <span className="absolute top-[57px] left-[34px] w-[348px] h-[15px] text-[13px] font-medium leading-[16px] tracking-[0.1em] text-[#5F5F5F]">
+        스테이블코인 핵심 지표를 실시간으로 추적합니다.
+      </span>
+    </div>
   )
 }
